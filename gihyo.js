e03f1e08a404d9c11dc58cbc5fdaa90da24f7dae
smeghead <smeghead7@gmail.com>
2012-09-27 23:50:04 +0900

    if exists skip button, location.reload();

--- gihyo.js
+++ gihyo.js
@@ -2,7 +2,7 @@ console.log('gihyo.js: initialize start.');
 var do_skip = function(skip_link) {
   if (skip_link) {
     var evt = document.createEvent("MouseEvents");
-    evt.initEvent('click', false, true);
+    evt.initEvent('click', true, false);
     skip_link.dispatchEvent(evt);
     console.log('gihyo.js: click skip link.');
     var message = document.createTextNode();
@@ -13,6 +13,7 @@ var do_skip = function(skip_link) {
     var body = document.getElementsByTagName('body')[0];
     body.appendChild(div);
     console.log('gihyo.js: message has shown.');
+    location.reload();
   }
 };
 //技評
@@ -33,8 +34,9 @@ var mynavi_news = function(){
     if (iframe) {
       if (!iframe.contentDocument) continue;
 
-      var skip_mycom = iframe.contentDocument.querySelector('div#footer-btn-skip a');
+      var skip_mycom = iframe.contentDocument.querySelector('p.header_skip a');
       if (skip_mycom) {
+        console.log('found skip button.', skip_mycom);
         return skip_mycom;
       }
     }
@@ -57,7 +59,9 @@ var timer = setInterval(function(){
     clearInterval(timer);
     return;
   }
-  count++;
+  if (document.querySelector('body')) {
+    count++;
+  }
   console.log('count ' + count);
 }, 500);
 /* vim: set ts=2 sw=2 sts=2 expandtab fenc=utf-8: */
